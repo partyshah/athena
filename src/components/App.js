4494f881be0f0deab1bd673689bab6b884cd718b
Zachary Kirby <ARK@airbears2-10-142-173-24.airbears2.1918.berkeley.edu>
2017-07-19 13:59:57 -0700

    Updated files

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+
+	    return (
+	      <div>
+	      	<div>Hello Worlds</div>
+	      </div>
+	    );
+	}
+}
+
+export default App;
